dae91b17e055f66c23c1dd117587715de05df461
Alyson Matheus <alyson1907@gmail.com>
2020-05-08 14:01:49 -0300

    +contentContainerStyle to seet Carousel width using numberOfItems

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import { Animated, View, Text, ART } from 'react-native'
+import { Animated, View, Text, ART, Dimensions } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import styles from './Styles/Carousel'
 
 const Carousel = (props) => {
   const hasChildren = !!props.children
   const numberOfItems = hasChildren && props.children.length || 0
+
   const [selectedBullet, setSelectedBullet] = useState(0)
 
   const handleBullet = e => {
     const getItemIndex = e => {
       const { width: contentWidth } = e.nativeEvent.contentSize
       const { x: contentOffset } = e.nativeEvent.contentOffset
-  
+
       const pageWidth = parseInt(contentWidth / numberOfItems)
       const currentPage = Math.round(contentOffset / pageWidth)
-  
+
       return currentPage
     }
     const currentBullet = getItemIndex(e)
@@ -35,7 +36,9 @@ const Carousel = (props) => {
 
   return (
     <View style={props.style}>
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}
+      <ScrollView horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ width: `${100 * numberOfItems}%`, justifyContent: 'space-around' }}
         decelerationRate="normal"
         pagingEnabled
         onScroll={handleBullet}
