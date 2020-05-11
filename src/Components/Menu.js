9ef2b5cb530cb491eefa509f63f4ff223747621c
Alyson Matheus <alyson1907@gmail.com>
2020-05-11 12:01:50 -0300

    fixed Text import @MenuItem : from react-native --> Components

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 // Components
 import MenuItem from './MenuItem'
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from "react-native"
+import { View, Animated } from "react-native"
 import Text from '../Components/Text'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 // Components
