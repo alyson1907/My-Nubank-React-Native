c15a4b94b937f5693cf6a2bd6cb660243d911047
Alyson Matheus <alyson1907@gmail.com>
2020-05-12 10:39:25 -0300

    +initial Payment Page, +Page change animations @routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,44 @@ import { createStackNavigator } from '@react-navigation/stack'
 const Stack = createStackNavigator()
 // Pages
 import Home from './pages/Home'
+import Payment from './pages/Payment'
+import { Dimensions } from 'react-native'
 
 const Routes = () => {
+  const transitionAnimation = ({ current: { progress } }) => {
+    return {
+      // Styles applied to the background screen
+      overlayStyle: {
+        opacity: progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 0.4],
+          extrapolate: 'clamp',
+        })
+      },
+      // transition applied to the front screen opening
+      cardStyle: {
+        transform: [
+          {
+            translateY: progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [Dimensions.get('window').height, 0]
+            })
+          }
+        ]
+      }
+    }
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{
+        cardStyle: { backgroundColor: 'transparent' },
+        headerShown: false,
+        cardStyleInterpolator: transitionAnimation
+        // cardOverlayEnabled: true,
+      }} initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Payment" component={Payment} />
       </Stack.Navigator>
     </NavigationContainer>
   )
