7f91f24f0a84398c675c7791b88bc489ec603176
Alyson Matheus <alyson1907@gmail.com>
2020-05-12 10:55:41 -0300

    +navigation to MenuItem & Footercard

--- src/Components/FooterCard.js
+++ src/Components/FooterCard.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Image } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 import Text from './Text'
+import { Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 import Icon from 'react-native-vector-icons/AntDesign'
 import Colors from '../Colors'
 import styles from './Styles/FooterCard'
 
 const FooterCard = (props) => {
+  const navigation = useNavigation()
+  const navigateTo = pageName => props.navigateTo && navigation.navigate(pageName)
+
   return (
     // TODO add link to navigate to another screen
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={() => navigateTo(props.navigateTo)}>
       <Icon name={props.icon} size={22} color={Colors.secondary} />
       <Text style={styles.contentText}>{props.text}</Text>
     </TouchableOpacity>
