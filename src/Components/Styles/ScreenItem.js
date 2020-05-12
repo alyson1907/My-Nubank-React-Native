b4c9cab3370a85fa7da968ff6bc9066af2e1aedd
Alyson Matheus <alyson1907@gmail.com>
2020-05-12 11:48:01 -0300

    +ScreenItem

---
+++ src/Components/Styles/ScreenItem.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+import Colors from '../../Colors'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: Colors.fontSecondary,
+    borderBottomWidth: 1
+  },
+
+  txtContainer: {
+    paddingHorizontal: 18
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+
+  description: {
+    fontSize: 18
+  }
+})
+
+export default styles
