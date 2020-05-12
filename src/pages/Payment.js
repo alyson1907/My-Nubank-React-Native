7016fbf89158b87a12a2b11d07f1a8780e664610
Alyson Matheus <alyson1907@gmail.com>
2020-05-12 10:39:25 -0300

    +initial Payment Page, +Page change animations @routes.js

---
+++ src/pages/Payment.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View } from 'react-native'
+import Text from '../Components/Text'
+
+import styles from './Styles/Payment'
+
+const Payment = () => {
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text>SAODASDASDASD</Text>
+      </View>
+    </View>
+  )
+}
+
+export default Payment
---
+++ src/pages/Styles/Payment.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+import Colors from '../../Colors'
+
+const styles = StyleSheet.create({
+  container: {
+    height: '96%',
+    top: '10%',
+    borderTopLeftRadius: 40,
+    padding: 32,
+    backgroundColor: Colors.backgroundPrimary
+  }
+})
+
+export default styles
