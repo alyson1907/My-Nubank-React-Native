0df2a32e613cbdeddfda2d6200f071298527cadd
Alyson Matheus <alyson1907@gmail.com>
2020-05-12 10:55:41 -0300

    +navigation to MenuItem & Footercard

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { View } from 'react-native'
 import Text from './Text'
+import { View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+
+
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import styles from './Styles/MenuItem'
 
 const MenuItem = (props) => {
+  const navigation = useNavigation()
+  const navigateTo = pageName => props.navigateTo && navigation.navigate(pageName)
+
   return (
     <View style={styles.item}>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={() => navigateTo(props.navigateTo)}>
         <Icon name={props.icon} style={styles.icon} size={22} />
         <Text style={styles.text}>{props.text}</Text>
       </TouchableOpacity>
