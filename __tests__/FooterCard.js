69a6c8f4bcc58b6b1c34839a00a309d788e7d614
Alyson Matheus <alyson1907@gmail.com>
2020-05-14 14:41:00 -0300

    +snapshot test for Footercard

--- __tests__/FooterCard.js
+++ __tests__/FooterCard.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import 'react-native'
-import { shallow, mount, render } from 'enzyme'
+import { mount } from 'enzyme'
+import renderer from 'react-test-renderer'
 import FooterCard from '../src/Components/FooterCard'
 
+const globals = {
+  text: 'random test text',
+  icon: 'creditcard'
+}
+
 describe('HomeScreen Footer List Tests', () => {
+  it('evaluates the rendered card with snapshot', () => {
+    const tree = renderer.create(<FooterCard  icon={globals.icon} text={globals.text} />)
+    expect(tree).toMatchSnapshot()
+  })
+
   it('renders single card correctly', () => {
-    const text = 'random test text'
     // Mounting Component
-    const wrapper = mount(<FooterCard icon='creditcard' text={text} />)
+    const wrapper = mount(<FooterCard icon={globals.icon} text={globals.text} />)
     const footerBtn = wrapper.find("[testID='footer-card']").first()
     const footerTxt = wrapper.find("[testID='footer-card-text']").first().text()
 
     expect(footerBtn).toBeTruthy()
-    expect(footerTxt).toEqual(text)
+    expect(footerTxt).toEqual(globals.text)
   })
 })
