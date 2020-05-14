a31cdfafc1c33228a29422935d12b025293866ea
Alyson Matheus <alyson1907@gmail.com>
2020-05-14 12:56:22 -0300

    +test to check children in Carousel

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,10 @@
 jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon')
 jest.mock('react-native-vector-icons/FontAwesome5', () => 'Icon')
 jest.mock('react-native-vector-icons/AntDesign', () => 'Icon')
+
+// ENZYME
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import 'jsdom-global/register'
+
+Enzyme.configure({ adapter: new Adapter() })
