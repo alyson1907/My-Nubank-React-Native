1d2d6024f7385416ef1a381208797c2fb14cd018
Alyson Matheus <alyson1907@gmail.com>
2020-05-15 14:00:58 -0300

    WIP

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
+      "babel-preset-expo",
       "@babel/preset-env",
       "@babel/preset-react",
       // preset-flow must be the last
