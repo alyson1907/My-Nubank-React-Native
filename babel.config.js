76a0a394dd3e3c01a7f5e9a69ec6947d75a9328c
Alyson Matheus <alyson1907@gmail.com>
2020-05-15 12:42:40 -0300

    commented test/App.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      // preset-flow must be the last
+      "@babel/preset-flow"
+    ],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      ["@babel/plugin-transform-modules-commonjs"]
+    ]
   };
 };
