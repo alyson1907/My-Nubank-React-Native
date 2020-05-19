dffc1010cd0695e4de89c37c3595f39eb9d9e5a4
Alyson Matheus <alyson1907@gmail.com>
2020-05-19 19:02:55 -0300

    .

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,12 @@ module.exports = function (api) {
   return {
     presets: [
       "babel-preset-expo",
-      "@babel/preset-env",
       "@babel/preset-react",
       // preset-flow must be the last
       "@babel/preset-flow"
     ],
     plugins: [
       ["@babel/plugin-proposal-decorators", { legacy: true }],
-      ["@babel/plugin-proposal-class-properties", { loose: true }],
       ["@babel/plugin-transform-modules-commonjs"]
     ]
   };
